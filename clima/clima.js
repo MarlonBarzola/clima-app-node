817e805e32ed8f0eed39fe14532d20fecb949357
Marlon <marlonbarzola@hotmail.com>
2020-01-08 20:42:33 -0500

    Segundo commit

--- clima/clima.js
+++ clima/clima.js
@@ -4,7 +4,7 @@ const getClima = async (lat, lon) => {
 
     const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lon }&appid=4973bce3290ebec429248fbba33d382a&units=metric`);
 
-    if(!resp.data.length === 0) {
+    if(resp.data.length === 0) {
         throw new Error(`No hay resultados para latitud: ${ lat } y longitud: ${ lon }`);
     }
 
